e1a60bd20e7b10fd4d1258f19d4957fb43982dbd
eslam <eslammongy995@gmail.com>
2022-03-20 22:14:53 +0200

    fixing updating product issue 01

--- admin.php
+++ admin.php
@@ -1,37 +1,38 @@
-<?php 
+<?php
 error_reporting(E_ERROR | E_PARSE);
 include('config.php');
-
-if(isset($_POST['add-product-btn'])){
+/* adding new product */
+if (isset($_POST['add-product-btn'])) {
     $proName = $_POST['pro_name'];
     $proPrice = $_POST['pro_price'];
     $proImage = $_FILES['pro_image']['name'];
     $proImageTempName = $_FILES['pro_image']['tmp_name'];
-    $proImageFolder = 'uploaded_img/'.$proImage;
+    $proImageFolder = 'uploaded_img/' . $proImage;
 
-     $query = "INSERT INTO products(name,price,image)
+    $query = "INSERT INTO products(name,price,image)
         VALUES('$proName','$proPrice','$proImage')";
     $addingNewProduct = mysqli_query($db_connection, $query) or die('error occured when adding new product');
 
-    if($addingNewProduct){
+    if ($addingNewProduct) {
         move_uploaded_file($proImageTempName, $proImageFolder);
         $message[] = 'adding new product successfully';
-    }else{
+    } else {
         $message[] = "could not add new product";
     }
 };
-if(isset($_GET['delete'])){
+/* deleting selected product */
+if (isset($_GET['delete'])) {
     $deleteID = $_GET['delete'];
     $deleteQuery = "DELETE FROM products WHERE id = $deleteID";
     $deleteProduct = mysqli_query($db_connection, $deleteQuery);
-    if($deleteProduct){
+    if ($deleteProduct) {
         // header('location:admin.php');
         $message[] = "product deleted successfully";
-    }else{
+    } else {
         //   header('location:admin.php');
         $message[] = "product deleted successfully";
     }
-}
+};
 
 ?>
 
@@ -48,11 +49,11 @@ if(isset($_GET['delete'])){
 </head>
 
 <body>
-    <?php 
-    if(isset($message)){
-foreach($message as $message){
-    echo '<div class="message"><span>'.$message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
-}
+    <?php
+    if (isset($message)) {
+        foreach ($message as $message) {
+            echo '<div class="message"><span>' . $message . '</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
+        }
     };
     ?>
 
@@ -80,12 +81,12 @@ foreach($message as $message){
                     <th>Action</th>
                 </thead>
                 <tbody>
-                    <?php 
+                    <?php
                     $selectQuery = "SELECT * FROM products ORDER BY productDate DESC";
                     $selectProducts = mysqli_query($db_connection, $selectQuery);
-                    if(mysqli_num_rows($selectProducts) > 0){
-                        while($row = mysqli_fetch_assoc($selectProducts)){
-                     ?>
+                    if (mysqli_num_rows($selectProducts) > 0) {
+                        while ($row = mysqli_fetch_assoc($selectProducts)) {
+                    ?>
                     <tr>
                         <td><?php echo $row['name']; ?></td>
                         <td><?php echo $row['price']; ?>$</td>
@@ -94,17 +95,16 @@ foreach($message as $message){
                         <td><a href="admin.php?delete=<?php echo $row['id']; ?>" class="delete-btn"
                                 onclick="return confirm('are you sure you want to delete this product?');">
                                 <i class="fas fa-trash"></i>Delete</a>
-                            <a href="admin.php?edit=<?php echo $row['id']; ?>" class="option-btn"
-                                onclick="return confirm('are you sure you want to delete this product?');">
-                                <i class="fas fa-edit"></i>Update</a>
+                            <a href="admin.php?edit=<?php echo $row['id']; ?>" class="option-btn"> <i
+                                    class="fas fa-edit"></i> update </a>
                         </td>
                     </tr>
 
 
                     <?php
-                      };
-                    }else{
-                        echo "<sapn>No Products Added Yet</span>";
+                        };
+                    } else {
+                        echo "<div class='empty'>No Products Added Yet</div>";
                     }
                     ?>
 
@@ -113,11 +113,47 @@ foreach($message as $message){
 
 
         </section>
+        <!-- editing in selected product -->
+        <section class="edit-form-container">
+
+            <?php
+   
+   if(isset($_GET['edit'])){
+      $edit_id = $_GET['edit'];
+       $editQuery = "SELECT * FROM products WHERE id = $edit_id";
+   
+      $edit_query = mysqli_query($db_connection, $editQuery);
+      if(mysqli_num_rows($edit_query) > 0){
+         while($fetch_edit = mysqli_fetch_assoc($edit_query)){
+   ?>
+
+            <form action="" method="post" enctype="multipart/form-data">
+                <img src="uploaded_img/<?php echo $fetch_edit['image']; ?>" height="200" alt="">
+                <input type="hidden" name="update_p_id" value="<?php echo $fetch_edit['id']; ?>">
+                <input type="text" class="box" required name="update_p_name" value="<?php echo $fetch_edit['name']; ?>">
+                <input type="number" min="0" class="box" required name="update_p_price"
+                    value="<?php echo $fetch_edit['price']; ?>">
+                <input type="file" class="box" required name="update_p_image" accept="image/png, image/jpg, image/jpeg">
+                <input type="submit" value="update the prodcut" name="update_product" class="btn">
+                <input type="reset" value="cancel" id="close-edit" class="option-btn">
+            </form>
+
+            <?php
+            };
+         };
+         echo "<script>document.querySelector('.edit-form-container').style.display = 'flex';</script>";
+      };
+   ?>
+
+        </section>
+
 
     </div>
 
     <!-- js file link -->
-    <script src="js/script.js"></script>
+    <script src="js/script.js">
+
+    </script>
 </body>
 
 </html>
