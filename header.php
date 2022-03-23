cc39535f864a1da4b33561eb3cd287d2c6a01c64
eslam <eslammongy995@gmail.com>
2022-03-23 10:43:41 +0200

    adding product to cart

--- header.php
+++ header.php
@@ -6,7 +6,12 @@
             <a href="admin.php">Add Product</a>
             <a href="products.php">View Products</a>
         </nav>
-        <a href="cart.php" class="cart">Cart <span>0</span></a>
+        <?php 
+        @include 'config.php';
+        $selectCartNum = mysqli_query($db_connection, "SELECT * FROM cart") or die('Query Failed');
+        $row_count = mysqli_num_rows($selectCartNum);
+        ?>
+        <a href="cart.php" class="cart">Cart <span><?php echo $row_count; ?></span></a>
         <div id="menu-btn" class="fas fa-bars">
 
         </div>
--- products.php
+++ products.php
@@ -1,5 +1,23 @@
 <?php
 @include 'config.php'; 
+
+if(isset($_POST['add_to_cart'])){
+    $product_name = $_POST['product_name'];
+    $product_price = $_POST['product_price'];
+    $product_image = $_POST['product_image'];
+    $producty_quantity =1;
+
+    $selectQuery = "SELECT * FROM  cart WHERE name = '$product_name'";
+    $selectProCart = mysqli_query($db_connection, $selectQuery);
+    if(mysqli_num_rows($selectProCart) > 0){
+        $message[] = "product already added to cart";
+    }else{
+         $insertQuery = "INSERT INTO cart(name, price, image, quantity) VALUES('$product_name', '$product_price', '$product_image', '$producty_quantity')";
+         $insertToCart = mysqli_query($db_connection, $insertQuery);
+         $message[] = "product added to cart cart successfully";
+    }
+
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -37,7 +55,7 @@
                         <img src="uploaded_img/<?php echo $product['image']; ?>" alt="">
                         <h3><?php echo $product['name']; ?></h3>
                         <div class="price">
-                            <?php echo $product['price']; ?>
+                            $<?php echo $product['price']; ?>/-
                         </div>
                         <input type="hidden" name="product_name" value="<?php echo $product['name']; ?>">
                         <input type="hidden" name="product_price" value="<?php echo $product['price']; ?>">
